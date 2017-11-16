2d8c4827f8221d1f1c9cd684229b2d877c81d599
min-ploy <32128262+min-ploy@users.noreply.github.com>
2017-11-16 22:17:55 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -213,7 +213,6 @@ if (!is_null($events['events'])) {
 							'type' => 'carousel',
 							'columns' => [
 								[
-									'imageurl' => "https://cdn.shopify.com/s/files/1/0379/7669/products/sampleset2_1024x1024.JPG?v=1458740363",
 									'title' => "$name[0]",
 									'text' => "$address[0]",
 									'actions' => [
