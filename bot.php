bc458ba9fe7946dad9d4b5e14248f7eb335b2bf9
min-ploy <32128262+min-ploy@users.noreply.github.com>
2017-11-17 10:14:09 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -216,6 +216,7 @@ if (!is_null($events['events'])) {
 							'type' => 'carousel',
 							'columns' => [
 								[
+									'imageUrl' => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4c0C88GdNvz0GjS5VhUajxdsI1c3ZBd5VBxygh0p7A0_180JV"
 									'title' => "$name[0]",
 									'text' => "$address[0]",
 									'actions' => [
